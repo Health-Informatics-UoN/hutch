fd54f73f08ed7229984ae3ca6eab2e34c1b7e83b
Andy Rae <hello@rae.li>
2025-05-17 18:53:40 +0100

    Change order

--- website/pages/concepts/_meta.js
+++ website/pages/concepts/_meta.js
@@ -1,6 +1,6 @@
 export default {
   cohort_discovery: "Cohort Discovery",
+  disclosure_controls: "Disclosure Controls",
   availability: "Availability Task",
   distribution: "Distribution Task",
-  disclosure_controls: "Disclosure Controls",
 };
