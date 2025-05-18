1efa75316eae48302c97b1167c665abc5692ba00
Andy Rae <hello@rae.li>
2025-05-18 14:34:31 +0100

    Fix caseing import

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useConfig } from "nextra-theme-docs";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default {
   docsRepositoryBase:
